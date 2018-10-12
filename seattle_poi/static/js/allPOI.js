624c450289a38cea5f0ead10adbafcb7386135f1
NCJo <cnimkulr@gmail.com>
2018-10-11 23:55:12 -0600

    Added: All the .js files have been added

---
+++ seattle_poi/static/js/allPOI.js
@@ -0,0 +1,68 @@
+var myPOIs = [
+    {
+      title: 'Space Needle',
+      lat: 47.620506,
+      lng: -122.349277,
+      address: '400 Broad St, Seattle, WA 98109'
+    },
+    {
+      title: 'Gum Wall',
+      lat: 47.608305,
+      lng: -122.340224,
+      address: '1428 Post Alley, Seattle, WA 98101'
+    },
+    {
+      title: 'Smith Tower',
+      lat: 47.601980,
+      lng: -122.331717,
+      address: '506 2nd Ave, Seattle, WA 98104'
+    },
+    {
+      title: 'Seattle Center',
+      lat: 47.620745,
+      lng: -122.350433,
+      address: '305 Harrison St, Seattle, WA 98109'
+    },
+    {
+      title: 'Seattle Art Museum',
+      lat: 47.607309,
+      lng: -122.338133,
+      address: '1300 1st Ave, Seattle, WA 98101'
+    },
+    {
+      title: 'Olympic Sculpture Park',
+      lat: 47.616399,
+      lng: -122.354103,
+      address: '2901 Western Ave, Seattle, WA 98121'
+    },
+    {
+      title: 'Wing Luke Museum',
+      lat: 47.598140,
+      lng: -122.322804,
+      address: '719 S King St, Seattle, WA 98104'
+    },
+    {
+      title: 'Ye Olde Curiousity Shop',
+      lat: 47.604128,
+      lng: -122.339603,
+      address: 'Pier 54, 1001 Alaskan Way, Seattle, WA 98104'
+    },
+    {
+      title: 'Seattle Central Library',
+      lat: 47.606600,
+      lng: -122.332984,
+      address: '1000 4th Ave, Seattle, WA 98104'
+    },
+    {
+      title: 'Seattle Aquarium',
+      lat: 47.607257,
+      lng: -122.343753,
+      address: '1483 Alaskan Way, Seattle, WA 98101'
+    },
+    {
+      title: 'Benaroya Hall',
+      lat: 47.608075,
+      lng: -122.336915,
+      address: '200 University St, Seattle, WA 98101'
+    },
+  ]
